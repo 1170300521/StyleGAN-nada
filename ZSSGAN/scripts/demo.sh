fed136097eb243477b00399d93906d7ac704d8b6
1170300521 <hitzhangyabo2017@163.com>
2021-12-14 11:51:01 +0800

    add comment for different dataset

--- ZSSGAN/scripts/demo.sh
+++ ZSSGAN/scripts/demo.sh
@@ -13,11 +13,12 @@
 #                --clip_model_weights 1.0 1.0 \
 #                --mixing 0.0 \
 #                --save_interval 150 \
-CUDA_VISIBLE_DEVICES=0 python train.py --size 1024 \
+# ffhq: 1024; cat: 512; dog: 512; church: 256; horse: 256; car: 512, crop_for_cars 
+CUDA_VISIBLE_DEVICES=0 python train.py --size 512 \
                 --batch 2 \
-                --n_sample 4 --output_dir ../results/ffhq/pca_latest_16_disney_princess\
+                --n_sample 4 --output_dir ../results/church/pca_latest_16_disney_princess\
                 --lr 0.002 \
-                --frozen_gen_ckpt ../weights/stylegan2-ffhq-config-f.pt \
+                --frozen_gen_ckpt ../weights/stylegan2-car-config-f.pt \
                 --iter 301 \
                 --source_class "photo" \
                 --target_class "Disney Princess" \
@@ -28,6 +29,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py --size 1024 \
                 --save_interval 1500 \
                 --clip_models "ViT-B/32" \
                 --clip_model_weights 1.0 \
+                --crop_for_cars
 		#--mixing 0.9
 #CUDA_VISIBLE_DEVICES=7 python train.py --size 1024 \
 #                --batch 2 \
